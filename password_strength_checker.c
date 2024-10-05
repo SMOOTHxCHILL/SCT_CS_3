4432979248bb0965a6cc5f04736972da8705ecfe
SMOOTHxCHILL <anirudh0706@gmail.com>
2024-10-05 16:24:58 +0530

    Update password_strength_checker.c

--- password_strength_checker.c
+++ password_strength_checker.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main() {
-    char pass;
+    char pass[100];
+    int i, lnt = 0, upr = 0, lwr = 0, sym = 0, num = 0;
     printf("Enter your to assess its quality:\n");
     scanf("%s", &pass);
-    for (i in pass) {
-        int lnt += 1;
-        if (isupper(i)) {
-            int upr += 1;
+    for (i = 0; pass[i] != '\0'; i++) {
+        lnt += 1;
+        if (isupper(pass[i])) {
+            upr++;
         }
-        if (islower(i)) {
-            int lwr += 1;
+        if (islower(pass[i])) {
+            lwr++;
         }
-        if (isalpha(i) not True) {
-            if (isspace(i) not True) {
-                int sym += 1;
+        if (!isalpha(pass[i])) {
+            if (!isspace(pass[i])) {
+                if (!isdigit(pass[i])) {
+                    sym++;
+                }
             }
         }
-        if (isdigit(i)) {
-            int num += 1;
+        if (isdigit(pass[i])) {
+            num++;
         }
-        printf("Length:%d", lnt);
-        printf("Uppercase letters:%d", upr);
-        printf("Lowercase letters:%d", lwr);
-        printf("Symbols:%d", sym);
-        printf("Numbers:%d", num);
     }
+    printf("Length:%d\n", lnt);
+    printf("Uppercase letters:%d\n", upr);
+    printf("Lowercase letters:%d\n", lwr);
+    printf("Symbols:%d\n", sym);
+    printf("Numbers:%d\n", num);
 }
